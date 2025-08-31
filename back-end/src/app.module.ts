c1ef80d0e245d3943e16d5703aeca0982b8ec91a
Manassés Raimundo <manassesraimundo.mr@gmail.com>
2025-08-31 18:30:01 +0200

    feat(auth): add module, controller and service

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { EmailModule } from './email/email.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, EmailModule, AuthModule, EmailModule],
+  imports: [PrismaModule, EmailModule, AuthModule, UserModule],
 })
 export class AppModule {}
