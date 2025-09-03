a15d3d0bc0513d4abf52727b7037fc642084bc66
Manassés Raimundo <manassesraimundo.mr@gmail.com>
2025-09-03 20:02:17 +0200

    feat(user): create user module, controller and service

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { EmailModule } from './email/email.module';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [PrismaModule, EmailModule, AuthModule, UserModule],
+  imports: [PrismaModule, EmailModule, AuthModule, UserModule, ProductModule],
 })
 export class AppModule {}
