eacc52d219391ea4cfd0c61c9d97895f6ab63707
manasses <pro@Manasses.local>
2025-12-07 15:35:46 +0100

    modified

--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -11,7 +11,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthSinUpDto, AuthSinInDto, UserRole } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt';
 import { EmailService } from 'src/email/email.service';
-
+ 
 @Injectable()
 export class AuthService {
   constructor(
